5b137195f26562b4943f0bbf76d4183feda54792
yigabriel <gabrielsacc7@gmail.com>
2025-08-10 12:38:51 -0500

    feat: add signup and sign in page

--- src/router/index.js
+++ src/router/index.js
@@ -11,8 +11,10 @@ import {createRouter, createWebHistory} from "vue-router";
  * Using dynamic imports to enable code splitting and improve initial load performance
  */
 
-const HomeComponent = () => import('../public/pages/home.component.vue');
-const PageNotFoundComponent = () => import('../public/pages/page-not-found.component.vue');
+// const HomeComponent = () => import('../public/pages/home.component.vue');
+const SignInComponent = () => import("../public/pages/signin.component.vue");
+const SignUpComponent = () => import("../public/pages/signup.component.vue");
+
 
 /**
  * @type {import('vue-router').RouteRecordRaw[]}
@@ -24,10 +26,11 @@ const PageNotFoundComponent = () => import('../public/pages/page-not-found.compo
  * - meta: Additional metadata including page title
  */
 const routes = [
-    {   path: '/home',                  name: 'home',       component: HomeComponent,               meta: {title: 'Home'}},
-    {   path: '/about',                 name: 'about',      component: AboutComponent,              meta: {title: 'About us'}},
-     {   path: '/',                      name: 'default',    redirect: {name: 'home'}},
-    {   path: '/:pathMatch(.*)*',       name: 'not-found',  component: PageNotFoundComponent,       meta: {title: 'Page not found'}},
+    {   path: '/signIn', name: 'signIn',component: SignInComponent, meta: {title: 'SignIn'}},
+    {   path: '/signUp', name: 'signUp',component: SignUpComponent, meta: {title: 'SignUp'}},
+//    {   path: '/about',                 name: 'about',      component: AboutComponent,              meta: {title: 'About us'}},
+     {   path: '/', name: 'default', redirect: {name: 'SignIn'}},
+//    {   path: '/:pathMatch(.*)*',       name: 'not-found',  component: PageNotFoundComponent,       meta: {title: 'Page not found'}},
 ]
 
 /**
@@ -54,7 +57,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     console.log(`Navigating from ${from.name} to ${to.name}`);
     // Set the page title
-    let baseTitle = 'ACME Learning Center';
+    let baseTitle = 'TickIT';
     document.title = `${baseTitle} | ${to.meta['title']}`;
     next();
 });
---
+++ src/shared/auth-form.vue
@@ -0,0 +1,113 @@
+<script>
+
+import {ref} from "vue";
+import {Button as PvButton} from "primevue";
+
+export default{
+  name: "auth-form",
+  components: {PvButton},
+  props: {
+    resolver: Function,
+    fields: Array,
+    onFormSubmit: Function,
+    submitButton: String,
+    slack_label: String
+  },
+  data(){
+/*    return {
+      inputText: ref({
+        handle: {
+          borderRadius: '4px'
+        },
+        colorScheme: {
+          light: {
+            root: {
+              background: '{amber.500}',
+              checkedHoverBackground: '{amber.600}',
+              borderRadius: '4px'
+            },
+            handle: {
+              checkedBackground: '{amber.50}',
+              checkedHoverBackground: '{amber.100}'
+            }
+          }
+        }
+      })
+    }*/
+  }
+
+
+
+}
+</script>
+
+
+<template>
+    <div class="form" >
+      <pv-form :resolver="resolver" @submit="onFormSubmit" class="formFields">
+        <pv-form-field v-for="field in fields" :key="field.name" v-slot="$field" as="section" :name="field.name" :initialValue="field.initialValue" >
+          <label :for="field.name">{{ field.label }}</label>
+          <pv-input-text  v-if="field.type === 'text' || field.type === 'number'" :type="field.inputType" :placeholder="field.placeholder" fluid />
+          <pv-password    v-if="field.type === 'password'" :type="field.inputType" :placeholder="field.placeholder" :feedback="false" toggleMask fluid />
+          <pv-message     v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</pv-message>
+        </pv-form-field>
+        <div class="buttons">
+        <pv-button class="button" type="submit" :label="submitButton" />
+        <pv-button
+            @click="$emit('slack-event')"
+            severity="secondary">
+          <img src="@/assets/slack.svg" alt="slack icon" style="width: 17px">
+          <span>{{slack_label}}</span>
+        </pv-button>
+        </div>
+      </pv-form>
+    </div>
+</template>
+
+<style scoped>
+
+.buttons{
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5vh;
+  width: 100%;
+}
+
+.button{
+  display: flex;
+  width: 100%;
+  max-height: 40px;
+}
+
+.button p{
+  margin: 0;
+  padding: 0;
+}
+
+label{
+  font-weight: 450;
+  font-size: 15px;
+  display: block;
+  margin-bottom: 5px;
+  color: var(--contrast-gray);
+}
+
+.formFields{
+  display: flex;
+  flex-direction: column;
+  gap: 1.5vh;
+}
+
+.form{
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+}
+
+.form pv-input-text{
+  width: 100%;
+}
+</style>
