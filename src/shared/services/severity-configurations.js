b1917def1cfdbbe8df1f1873248ae05d68096ece
yigabriel <gabrielsacc7@gmail.com>
2025-09-04 00:36:20 -0500

    chore: add severity configurations for tags

---
+++ src/shared/services/severity-configurations.js
@@ -0,0 +1,14 @@
+
+export const severityConfig = {
+    critical: 'contrast' ,
+    high:     'danger' ,
+    medium:   'warn' ,
+    low:      'info'
+};
+
+export const statusConfig = {
+    open:           'danger' ,
+    in_progress:    'warn' ,
+    on_hold:        'secondary',
+    closed:         'success'
+};
