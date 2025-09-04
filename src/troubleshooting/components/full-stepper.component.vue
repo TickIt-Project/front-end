066a4edaef1bf60e50db113a939b730a448b2f3d
yigabriel <gabrielsacc7@gmail.com>
2025-09-03 19:08:42 -0500

    chore: add component reusability logic for report-issue-card.component.vue

--- src/troubleshooting/components/full-stepper.component.vue
+++ src/troubleshooting/components/full-stepper.component.vue
@@ -4,6 +4,7 @@ import {FileUpload as PvFileUpload, Select as PvSelect, Tag as PvTag} from "prim
 import {FormField as PvFormField} from "@primevue/forms";
 import {severityConfig} from "@/shared/services/severity-configurations.js";
 import ReportIssueCard from "@/shared/components/report-issue-card.component.vue";
+import {IssueReportAssembler} from "@/shared/services/report-assembler.js";
 
 
 export default {
@@ -14,38 +15,28 @@ export default {
       formValues: {
         name: '',
         description: '',
-        screen_issue: '',
+        screen: '',
         url: '',
-        role: '',
+        company_role: '',
         severity: {},
       },
       //todo change url to service
       uploadUrl: 'https://httpbin.org/post',
       value: '',
-      maxChars: 3000,
-      severityMap: {
-        critical: 'danger',
-        high: 'warn',
-        medium: 'info',
-        low: 'success'
-      },
-      severityMapLabel: {
-        critical: 'Critical',
-        high: 'High',
-        medium: 'Medium',
-        low: 'Low'
-      },
+      maxCharsDesc: 3000,
+      maxCharsTitle: 100,
       reportService: new IssueReportService(),
+      reportAssembler: new IssueReportAssembler(),
       /**
        * @property {Array<Object>} fields
        * @description Defines the structure of form fields to be rendered dynamically
        */
       fields: [
-        { name: 'name',       label: this.$t('report.steps.1.fields.title'),          type: 'text', inputType: 'text',  placeholder: this.$t('report.steps.1.fields.title'), initialValue: '' },
+        { name: 'title',       label: this.$t('report.steps.1.fields.title'),          type: 'text', inputType: 'text',  placeholder: this.$t('report.steps.1.fields.title'), initialValue: '' },
         { name: 'description',label: this.$t('report.steps.1.fields.description'),    type: 'editor', inputType: 'text',  placeholder: this.$t('report.steps.1.fields.description'), initialValue: '' },
-        { name: 'screen_issue',        label: this.$t('report.steps.1.fields.screen_issue'),   type: 'select', inputType: 'select',  placeholder: this.$t('report.steps.1.fields.screen_issue'), initialValue: '', editable: true, options:[]},
+        { name: 'screen',        label: this.$t('report.steps.1.fields.screen_issue'),   type: 'select', inputType: 'select',  placeholder: this.$t('report.steps.1.fields.screen_issue'), initialValue: '', editable: true, options:[]},
         { name: 'url',        label: this.$t('report.steps.1.fields.url'),            type: 'text', inputType: 'text',  placeholder: this.$t('report.steps.1.fields.url'), initialValue: '' },
-        { name: 'role',        label: this.$t('report.steps.1.fields.role'),   type: 'select', inputType: 'select',  placeholder: this.$t('report.steps.1.fields.role'), initialValue: '', editable: false, options:[] },
+        { name: 'company_role',        label: this.$t('report.steps.1.fields.role'),   type: 'select', inputType: 'select',  placeholder: this.$t('report.steps.1.fields.role'), initialValue: '', editable: false, options:[] },
         { name: 'severity',        label: this.$t('report.steps.1.fields.severity'),   type: 'select', inputType: 'select',  placeholder: this.$t('report.steps.1.fields.severity'), initialValue: '', editable: false, options:[] },
       ]
     }
@@ -58,7 +49,7 @@ export default {
     ])
         .then(([rolesRes, severityRes, screenRes]) => {
           // role loading
-          const roleField = this.fields.find(f => f.name === 'role');
+          const roleField = this.fields.find(f => f.name === 'company_role');
           if (roleField && Array.isArray(rolesRes.data)) {
             roleField.options = rolesRes.data.map(role => ({
               label: role,
@@ -70,17 +61,16 @@ export default {
           const severityField = this.fields.find(f => f.name === 'severity');
           if (severityField && Array.isArray(severityRes.data)) {
             severityField.options = severityRes.data.map(sev => ({
-              label: this.severityMapLabel[sev],
+              label: this.$t(`status.${sev}`),
               value: sev
             }));
           }
 
           // screen
-          const screenField = this.fields.find(f => f.name === 'screen_issue');
+          const screenField = this.fields.find(f => f.name === 'screen');
           if (screenField && Array.isArray(screenRes.data)) {
             screenField.options = screenRes.data.map(loc => ({
-              label: loc,
-              value: loc
+              label: loc
             }));
           }
         })
@@ -89,19 +79,31 @@ export default {
         });
   },
   methods: {
-    onUpload(event) {
-      console.log('Archivo subido correctamente')
-    },
-    onError(event) {
-      console.error('Error al subir archivo:')
-    }
+    formValuesToEntity(formValues) {
+      let report = this.reportAssembler.EntityFromResponse({
+        id : null,
+        companyId : null,
+        title : formValues.title,
+        description : formValues.description,
+        screen : formValues.screen.label,
+        companyRole : formValues.company_role.value,
+        severity : formValues.severity.value,
+        imgUrl : 'https://preview.redd.it/pls-gib-to-me-facts-lore-about-miku-v0-sfbyk901c82d1.jpeg',
+        status : null,
+        reporter : null,
+        assignee : null,
+        resolvedAt : null,
+        submittedAt : null,
+        ticketOption : false}
+    );
+      console.log(formValues);
+      console.log(report);
+      return report;
+  }
   },
   computed: {
-    severityConfig() {
-      return severityConfig
-    },
-    nameLength() {
-      return (this.formValues.name || '').trim().length;
+    titleLength() {
+      return (this.formValues.title || '').trim().length;
     },
 
     descPlainLength() {
@@ -109,45 +111,44 @@ export default {
       return html.replace(/<[^>]*>/g, '').trim().length;
     },
 
-
     isOverLimit() {
-      return this.nameLength > 100 || this.descPlainLength > 3000;
+      return this.titleLength > this.maxCharsTitle || this.descPlainLength > this.maxCharsDesc;
     },
 
     requiredFieldsFilled() {
       const urlRequired = this.isScreenIssueTyped;
       return (
-          this.nameLength >= 3 &&
-          this.descPlainLength > 0 &&
-          !!this.formValues.screen_issue &&
-          !!this.formValues.role &&
+          this.titleLength >= 3 &&
+          this.descPlainLength > 3 &&
+          !!this.formValues.screen &&
+          !!this.formValues.company_role &&
           !!this.formValues.severity &&
           (!urlRequired || (this.formValues.url && this.formValues.url.trim() !== ''))
       );
     },
 
     isScreenIssueTyped() {
-      const field = this.fields.find(f => f.name === 'screen_issue');
+      const field = this.fields.find(f => f.name === 'screen');
       if (!field) return false;
-      const currentValue = this.formValues.screen_issue || '';
+      const currentValue = this.formValues.screen || '';
       const match = field.options.some(opt => opt.value === currentValue);
       return !match && currentValue.trim() !== '';
     },
 
     formIsValid() {
       return this.requiredFieldsFilled && !this.isOverLimit;
-    }
-  },
+    },
 
+  },
   watch: {
     formValues: {
       handler() {
         console.log('formValues', this.formValues);
-        console.log('nameLength', this.nameLength, 'descPlainLength', this.descPlainLength, 'isOverLimit', this.isOverLimit, 'requiredFilled', this.requiredFieldsFilled, 'formIsValid', this.formIsValid);
+        console.log('titleLength', this.titleLength, 'descPlainLength', this.descPlainLength, 'isOverLimit', this.isOverLimit, 'requiredFilled', this.requiredFieldsFilled, 'formIsValid', this.formIsValid);
       },
       deep: true
     }
-  },
+  }
 }
 </script>
 
@@ -190,8 +191,8 @@ export default {
                     </template>
                 </pv-editor>
 
-                <p :style="{ color: descPlainLength > maxChars ? 'red' : 'inherit' }">
-                  {{ descPlainLength }} / {{ maxChars }} {{ $t('report.steps.1.characters') }}
+                <p :style="{ color: descPlainLength > maxCharsDesc ? 'red' : 'inherit' }">
+                  {{ descPlainLength }} / {{ maxCharsDesc }} {{ $t('report.steps.1.characters') }}
                 </p>
               </div>
 
@@ -254,7 +255,7 @@ export default {
         <pv-step>{{$t('report.steps.3.header')}}</pv-step>
         <pv-step-panel v-slot="{ activateCallback }">
           <div class="content">
-            <report-issue-card :border="true" :issue="formValues"></report-issue-card>
+            <report-issue-card :border="true" :issue="formValuesToEntity(formValues)"></report-issue-card>
           </div>
           <div class="buttons">
             <pv-button :label="$t('report.steps.buttons.back')" severity="secondary" @click="activateCallback('2')" />
