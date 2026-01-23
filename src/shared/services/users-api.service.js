e025365db5c6f57934e1d314a41fbe4f9940ac68
yigabriel <gabrielsacc7@gmail.com>
2026-01-23 16:58:26 -0500

    chore: option to change avatar

--- src/shared/services/users-api.service.js
+++ src/shared/services/users-api.service.js
@@ -10,4 +10,12 @@ export class UsersService {
         return http.get(`/it-members`);
     }
 
+    getUserById(id) {
+        return http.get(`/users/${id}`);
+    }
+
+    getCurrentUser() {
+        return http.get('/users/1');
+    }
+
 }
